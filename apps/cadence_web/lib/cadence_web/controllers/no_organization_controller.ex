f467ec7cc2513d3dd915cd1e43573b9440a5b092
Donovan Rost <donovan.rost@gmail.com>
2026-04-19 13:36:26 -0600

    feat(cadence_web): show pending invitations block on /no-organization

--- apps/cadence_web/lib/cadence_web/controllers/no_organization_controller.ex
+++ apps/cadence_web/lib/cadence_web/controllers/no_organization_controller.ex
@@ -2,8 +2,20 @@ defmodule CadenceWeb.NoOrganizationController do
   use CadenceWeb, :controller
 
   def show(conn, _params) do
+    pending_invitations_count = count_pending_invitations(conn.assigns[:current_scope])
+
     conn
     |> put_layout(html: {CadenceWeb.Layouts, :auth})
+    |> assign(:pending_invitations_count, pending_invitations_count)
     |> render(:show)
   end
+
+  defp count_pending_invitations(%Cadence.Auth.Scope{user: %{user_id: user_id}})
+       when is_binary(user_id) do
+    user_id
+    |> Cadence.list_notifications(only_unread: true)
+    |> Enum.count(&(&1.kind == :organization_invitation))
+  end
+
+  defp count_pending_invitations(_other), do: 0
 end
